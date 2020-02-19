8ce77ad929bbc034f2082074dd39a0c5a22fe240
nate-fleming <nfleming4@gmail.com>
2020-02-19 15:13:32 -0600

    created mock store

---
+++ src/index.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { render } from "@testing-library/react";
+import reducer from "../src/reducers/reducers";
+
+import App from "./App";
+
+const renderWithRedux = (
+  reducer: any,
+  initialState: any,
+  children: JSX.Element
+) => {
+  const mockStore = createStore(reducer, initialState);
+  return render(<Provider store={mockStore}>{children}</Provider>);
+};
+
+describe("<McgContainer/>", () => {
+  it("renders", () => {
+    const { asFragment } = renderWithRedux(reducer, {}, <App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
